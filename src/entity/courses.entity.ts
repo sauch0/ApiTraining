82177daf61223fc806db1040279af42be269dde5
Saumya <chitrakarsaumya2004@gmail.com>
2026-08-11 11:35:08 +0545

    created enrollment entity and migration

--- src/entity/courses.entity.ts
+++ src/entity/courses.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Enrollments } from "./enrollment.entity";
 
 @Entity()
 export class Courses {
@@ -13,4 +14,7 @@ export class Courses {
 
     @Column({ nullable: false })
     duration: string
+
+    @OneToMany(() => Enrollments, enrollment => enrollment.course)
+    enrollments: Enrollments[];
 }
